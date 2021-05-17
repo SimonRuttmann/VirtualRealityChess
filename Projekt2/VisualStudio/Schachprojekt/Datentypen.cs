518b16a3bb28683b490a946d81980eb6a7765542
ver0 <veronika_scheller@web.de>
2021-05-17 15:27:41 +0200

    Code hinzugefügt in dem Ordner VISUAL STUDIO

---
+++ Projekt2/VisualStudio/Schachprojekt/Datentypen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schachprojekt
+{
+    public class Position
+    {
+        public int xPos;
+        public int yPos;
+    }
+
+    
+}
